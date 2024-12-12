fb681a5315aa2d4aa6fe03fe8767a03fa53fd124
alneflibata <125027434+alneflibata@users.noreply.github.com>
2024-12-12 20:12:35 +0800

    Update diy-part1.sh

--- routine/diy-part1.sh
+++ routine/diy-part1.sh
@@ -21,26 +21,26 @@
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
 
 # 应用过滤
-#git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 
 # 京东签到
 #git clone https://github.com/noiver/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
 
 # 商店
-#git clone https://github.com/linkease/istore-ui package/istore-ui
-#git clone https://github.com/linkease/istore package/istore
+git clone https://github.com/linkease/istore-ui package/istore-ui
+git clone https://github.com/linkease/istore package/istore
 
 # 网易云解灰（天灵）
 #git clone https://github.com/UnblockNeteaseMusic/server package/server
 
 # 简单MESH
-#git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
+git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
 
 # 集客AC控制器
-#git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
+git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 
 # AdguardHome-app
-#git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 
 # MosDns
 #git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
