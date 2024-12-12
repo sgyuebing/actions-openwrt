67ec18152ef6b9d9c41c5f468dbedd930bc80e6a
alneflibata <125027434+alneflibata@users.noreply.github.com>
2024-12-12 20:16:12 +0800

    Update diy-part1.sh

--- simplify/diy-part1.sh
+++ simplify/diy-part1.sh
@@ -20,20 +20,6 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
 
-# 应用过滤
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-
-# 京东签到
-#git clone https://github.com/noiver/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
-
 # 商店
 git clone https://github.com/linkease/istore-ui package/istore-ui
 git clone https://github.com/linkease/istore package/istore
-
-# 网易云解灰（天灵）
-#git clone https://github.com/UnblockNeteaseMusic/server package/server
-
-# 简单mesh
-git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
-# 集客ac
-git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
