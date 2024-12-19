fb50484abb21d53fbdfc34550950c0e7ea314fda
alneflibata <125027434+alneflibata@users.noreply.github.com>
2024-12-19 21:29:46 +0800

    Update diy-part1.sh

--- my/diy-part1.sh
+++ my/diy-part1.sh
@@ -24,8 +24,9 @@
 git clone https://github.com/linkease/istore-ui package/istore-ui
 git clone https://github.com/linkease/istore package/istore
 
-# edge皮肤
+# 主题皮肤
 git clone https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+git clone https://github.com/derisamedia/luci-theme-alpha package/luci-theme-alpha
 
 # 集客AC控制器
 git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
